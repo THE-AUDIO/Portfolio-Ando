43d7a545714956067efb928153abb7545f967de0
THE-AUDIO <nandraina.dev22@gmail.com>
2025-01-04 15:02:50 +0300

    init first section

---
+++ Andorica/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'primary':'#0f0f0f',
+      'secondary':'#e3e3e3'
+    },
+  },
+  plugins: [],
+}
