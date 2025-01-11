540ee0dc61357478de3eba174146f71d526b4965
THE-AUDIO <nandraina.dev22@gmail.com>
2025-01-11 11:43:20 +0300

    add gsap

--- Andorica/src/app/app.component.ts
+++ Andorica/src/app/app.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
+  ngAfterViewInit(): void {
+    gsap.registerPlugin(ScrollTrigger)
+    gsap.from(".item", {
+      scale:0,
+      duration:1,
+      scrollTrigger:{
+        trigger: '.projet-content',
+        start: 'top 100%',
+        end:'top 40%',
+        scrub: 1,
+      }
+    })
+    gsap.to(".scrolling-text", {
+      x:2000,
+      duration:.1,
+      scrollTrigger:{
+        trigger: '.scrolling-text',
+        start: 'top 70%',
+        scrub: 1
+      }
+    })
+  }
   title = 'Andorica';
 }
